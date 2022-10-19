0772d5d1487c825b9a8065a77fc8c5512e0ab24d
fel <felicien.dehertogh@protonmail.com>
2022-10-19 19:54:12 +0200

    add get request to weather api

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const needle = require('needle')
+const url = require('url')
 
 const API_BASE_URL = process.env.API_BASE_URL
 const API_KEY_NAME = process.env.API_KEY_NAME
-const API_KE_VALUE = process.env.API_KE_VALUE
+const API_KEY_VALUE = process.env.API_KEY_VALUE
 
 router.get('/', async (req, res) => {
   try {
-    const apiRes = await needle('get', `${API_BASE_URL}`)
+    const params = new URLSearchParams({
+      // add api key name and value
+      [API_KEY_NAME]: API_KEY_VALUE,
+      // add query params from the request
+      ...url.parse(req.url, true).query,
+    })
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+    console.log(`${API_BASE_URL}?${params}`)
     const data = apiRes.body
+
+    // log request made to our public api
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
 
     res.status(200).json(data)
   } catch (error) {
